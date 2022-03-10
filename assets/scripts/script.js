36534b100ff70a76a9a5d6b5081213f6acd63485
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-10 13:26:00 +0100

    starting page

---
+++ assets/scripts/script.js
@@ -0,0 +1,19 @@
+function TicketPrice() {
+    let age = document.querySelector("#age").value;
+    let km = document.querySelector("#km").value;
+    let price = document.querySelector(".price");
+    let total = document.querySelector("#total");
+
+    price.style.display = "block";
+
+    finalprice = km * 0.21;
+    if (age < 18) {
+        finalprice = finalprice - finalprice * 20 / 100;
+    }
+    else if (age >= 65) {
+        finalprice = finalprice - finalprice * 40 / 100;
+    }
+
+    finalprice = Math.round(finalprice * 100) / 100;
+    total.innerHTML = `${finalprice} €`;
+}
